1327aaacbf76196b590ee5ffe599b11dbbf2f8e6
Benoit <59974364+benoitputzeys@users.noreply.github.com>
2020-06-05 17:02:15 +0200

    Cleaning up and trying to improve accuracy.

--- Temperature_LSTM/Temperature_LSTM.py
+++ Temperature_LSTM/Temperature_LSTM.py
@@ -1,11 +1,10 @@
-# Code here.
-# Recurrent Neural Network
+# Prediciting the temperature using a Recurrent Neural Network (LSTM to be more precise)
 # Part 1 - Data Preprocessing
-
-# Importing the libraries
+# Importing libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from keras.optimizers import RMSprop
 
 # Importing the training set
 df = pd.read_csv('https://raw.githubusercontent.com/jbrownlee/Datasets/master/daily-min-temperatures.csv')
@@ -18,8 +17,9 @@ df["Year"] = df.index.year
 df["Month"] = df.index.month
 df["Day"] = df.index.day
 
-# Reset the index.
+# Reset the index and drop the date column.
 df.reset_index(inplace = True)
+df = df.drop(columns = ["Date"])
 
 # Split the dataframe in training and testing set.
 nmb_rows_df = df.shape[0]
@@ -27,26 +27,22 @@ index = int(0.8*nmb_rows_df)
 train_df = df[0:index].copy()
 test_df = df[index:].copy()
 
-# Define the NN input features: Year, Month, Day and Temperature: Drop the Date columns.
-# As the temperature of the nex day will be predicted with the temperature from the previous day, the NN should also
-# be trained with the previous day temperatures.
-train_shifted_df = train_df.copy()
-train_shifted_df['Temp'] = train_shifted_df['Temp'].shift(periods = 1, fill_value = 0)
-train_shifted_df = train_shifted_df.drop(columns = ["Date"])
-Raw_Input = train_shifted_df.iloc[:, 0:4].values
+# Define the NN input features: Year, Month, Day and Temperature.
+# The temperature of the nex day will be predicted with the temperature from the 30 previous days.
+Raw_Input = train_df.iloc[:, 0:4].values
 Raw_Output = train_df[["Temp"]].values
 
 # Feature Scaling leaving it out for now. #todo Should I include this later?
-# from sklearn.preprocessing import MinMaxScaler
-# # Feature range equals (0, 1), that's the default feature range
-# # But also looking at the formula, Normalization gives you
-# # a range between 0 and 1.
-# scaler = MinMaxScaler(feature_range = (0, 1))
-# # fit_transform is a method of the MinMaxScaler. It will fit the object scaler to the training and it will transform it (scale it).
-# # More specifically, fit will get the minimum and maximum of the data.
-# scaled_training_set = scaler.fit_transform(Raw_Input)
+from sklearn.preprocessing import MinMaxScaler
+# Feature range equals (0, 1), that's the default feature range
+# But also looking at the formula, Normalization gives you
+# a range between 0 and 1.
+#scaler = MinMaxScaler(feature_range = (0, 1))
+# fit_transform is a method of the MinMaxScaler. It will fit the object scaler to the training and it will transform it (scale it).
+# More specifically, fit will get the minimum and maximum of the data.
 
-# Creating a data structure with 60 timesteps and 1 output
+
+# Creating a data structure with 30 previous observations and 1 output
 X_train = []
 y_train = []
 
@@ -55,15 +51,18 @@ y_train = np.zeros((len(Raw_Input)-30,1))
 for j in range(4):
     for i in range(30, len(Raw_Input)):
         for k in range(30):
-            X_train[j, i - 30, k] = Raw_Input[i-k, j]
+            X_train[j, i - 30, k] = Raw_Input[i-k-1, j]
             y_train[i - 30] = Raw_Input[i,0]
 
 # Reshaping
 # Keras requires an input of 3 dimensions
 # The first is the number of observations
-# The second is the number of timesteps
-# The third is the number of indicators.
+# The second is the number of observations we pass in from the past
+# The third enables the dates to be passed in as well.
 X_train = np.reshape(X_train, (X_train.shape[1], X_train.shape[2], X_train.shape[0]))
+
+# X_train = scaler.fit_transform(X_train)
+# y_train = scaler.fit_transform(y_train)
 
 # Part 2 - Building the RNN
 
@@ -77,43 +76,29 @@ from keras.layers import Dropout
 # Initialise the RNN as a sequence of layers as opposed to a computational graph.
 regressor = Sequential()
 
-# Adding the first LSTM layer and some Dropout regularisation
-# We need to input 3 arguments:
-# 1st, the number of units which is the number of LSTM cells or memory units
-# you want to have in this LSTM layer.
-# The 2nd input is return sequences, as we have more than 1 LSTM layer, you
-# have to set the retunr sequence argument to true. In the last LSTM layer,
-# you can omit this step because the default parameter is false which is what we want.
-# The 3rd and lat argument is the input shape which is the shape of the input containing x_train
-# It has an input shape of 3 dimensions corresponding to the obervations, the timesteps and
-# the indicators. We do not need to include the first dimension because the
-# number of observations will already be taken into account.
-# Remember the last 1 in the input_shape is the numbe rf indicators.
-
-# Because predicting the stock market price is pretty complex, you need to have a high dimensionality too thus 50
+# Because predicting the temperature is pretty complex, you need to have a high dimensionality too thus 50
 # for the number of neurons. If the number of neurons is too small in each of the LSTM layers, the model would not
 # capture very well the upward and downward trend.
-regressor.add(LSTM(units = 50, return_sequences = True, input_shape = (X_train.shape[1], X_train.shape[2])))
-regressor.add(Dropout(0.2))
+regressor.add(LSTM(units = 25, return_sequences = True, input_shape = (X_train.shape[1], X_train.shape[2])))
+regressor.add(Dropout(0.1))
 
-# Adding a second LSTM layer and some Dropout regularisation
+# Adding a second LSTM layer and Dropout regularisation
 # No need to specify any input shape here because we have already defined that we have 50 neurons in the
 # previous layer.
-regressor.add(LSTM(units = 50, return_sequences = True))
-regressor.add(Dropout(0.2))
+regressor.add(LSTM(units = 25, return_sequences = True))
+regressor.add(Dropout(0.1))
 
-# Adding a third LSTM layer and some Dropout regularisation
-regressor.add(LSTM(units = 50, return_sequences = True))
-regressor.add(Dropout(0.2))
+# Adding a third LSTM layer and Dropout regularisation
+regressor.add(LSTM(units = 25 ))
+regressor.add(Dropout(0.1))
 
-# Adding a fourth LSTM layer and some Dropout regularisation
-# This is the last LSTM layer that you are adding! Thus you need to say that the return sequences
-# are false. You are not going to return any more seuqences.
-regressor.add(LSTM(units = 50))
-regressor.add(Dropout(0.2))
+# # Adding a fourth LSTM layer and Dropout regularisation
+# # This is the last LSTM layer that is  added! Thus the return sequences is set to  false.
+# regressor.add(LSTM(units = 25))
+# regressor.add(Dropout(0.1))
 
 # Adding the output layer
-# We are not adding an LSTM layer! We are fully connecting the outward layer to the previous LSTM layer.
+# We are not adding an LSTM layer. We are fully connecting the outward layer to the previous LSTM layer.
 # As a result, we use a DENSE layer to make this full connection.
 regressor.add(Dense(units = 1))
 
@@ -122,76 +107,57 @@ regressor.add(Dense(units = 1))
 # But experimenting with other optimizers, one can also use the adam optimizer.
 # The adam optimizer is actually always a good choice and very powerfull too!
 # In general, the most commonly used optimizers are adam and RMSprop
-regressor.compile(optimizer = 'adam', loss = 'mean_squared_error')
+optimizer = RMSprop(lr = 1e-3)
+regressor.compile(optimizer = optimizer, loss = 'mean_squared_error')
 
 # Fitting the RNN to the Training set
-# Remember number of epochs is the number of times you want your whole training dataset to
-# be used in forward propagation and backpropagated to update the weights.
-# Also instead of updating the weights for every observation, update the weights after
-# 32 stock prize observation.
-regressor.fit(X_train, y_train, epochs = 45, batch_size = 32)
+regressor.fit(X_train, y_train, epochs = 20, batch_size = 64)
 
 # Part 3 - Making the predictions and visualising the results
 
-# Getting the real stock price of 2017
-test_temperatures = test_df[["Temp"]].values
+# Getting the real temperatures.
+test_temperatures = test_df.values
 
-# Getting the predicted stock price of 2017
-# For the test set, we also need the google stock market price of the 60 previous days.
+# For the test set, we also need the temperatures of the 30 previous days.
 # As a result, we need to concatinate the training and testing set.
-# One thing we should never do is to modify the actual test values. They have to remain how they are
-# Thus make sure to define a new variable containing the modified data and work with those.
-# With this new variable, you can scale it so that it corresponds to what the algorithm has
-# learned on. This new variable prohibits you from changing the test values.
-dataset_total = pd.concat((train_df['Temp'], test_df['Temp']), axis = 0)
-# Sidenote, you are training on 3 months because the google market value is given for each working day.
-# 60 working days / 5 is 12. This /4 is 3 months.
-# The way he got this was to think about the lower and upper bounds. To get the first day
+dataset_total = pd.concat((train_df, test_df), axis = 0)
+
+# To get the input values for the prediciton, think about the lower and upper bounds. For the first day
 # in the test set, you start at the bottom len(dataset_total) (which is the last day of the test set)
 # and then you substract the number of days in the test set len(dataset_test).
-# The first day requires the 60 previous days as well as an input which is why you need to
-# start even earlier and substract these 60 too. The value you just calculated is
-# the observation from which you want to start. From this value onward and all
-# the values further down (:) will be fed into your model to create a prediciton.
+# The first day requires the 30 previous days too (as an input) which is why you need to start even earlier and
+# substract these 30 too. The value we just calculated is the observation from which you want to start.
+# From this value onward and all the values further down (:) will be fed into the model to create a prediciton.
 # .values is just to make this a numpy array.
 inputs = dataset_total[len(dataset_total) - len(test_df) - 30:].values
-# The shape of inputs is (80,) but sklearn requires this to be in a shape (80,1)
-# Thus just put a reshape and all good.
-# inputs = inputs.reshape(-1,1)
-# # As you have trained on reshaped values, you have to reshape the inputs for the prediciton as well.
-# # Careful here to not use fit_transform as above because you do not want to compute the minimum and maximum
-# # again, you want to preserve those from before and subject your data to the same tranformation as before.
-# inputs = sc.transform(inputs)
 
-# Remember that in this step, you create a numpy array with 60 entries per rowand 20 columns
-# In each iteration, you add this row to the variable X_test via append.
-# After this, make sure to also make it an np.array.
-
-
+# Creating the 3D array for the test set similarly as before.
 X_test= np.zeros((4,len(inputs)-30,30))
 for j in range(4):
     for i in range(30, len(inputs)):
         for k in range(30):
-            X_test[j, i - 30, k] = inputs[i-k, j]
+            X_test[j, i - 30, k] = inputs[i-k-1, j]
 
-# # Just as above, reshape your input before it goes into the NN.
+# # Just as above, reshape the input before it goes into the NN.
 X_test = np.reshape(X_test, (X_test.shape[1], X_test.shape[2], X_test.shape[0]))
 # # Predict the stock prices.
-predicted_temperature = regressor.predict(X_test)
+#X_test = scaler.fit_transform(X_test)
+
 # # Rescale the output.
-# predicted_stock_price = sc.inverse_transform(predicted_stock_price)
+predicted_temperature = regressor.predict(X_test)
+# predicted_temperature = scaler.inverse_transform(predicted_temperature)
+
 
 # Visualising the results
-
 error = predicted_temperature - test_temperatures
 
-plt.plot(test_temperatures, color = 'red', label = 'Real Temperature')
-plt.plot(predicted_temperature, color = 'blue', label = 'Predicted Temperature')
-plt.plot(abs(error), color = 'black', label = 'Error')
+plt.plot(test_temperatures[:,0], color = 'red', label = 'Real Temperature')
+plt.plot(predicted_temperature[:,0], color = 'blue', label = 'Predicted Temperature')
+plt.plot(abs(error[:,0]), color = 'black', label = 'Error')
 plt.title('Temperature Prediction')
 plt.xlabel('Time')
 plt.ylabel('Temperature')
 plt.legend()
 plt.show()
 
-print("The asolute men error is ", abs(error)/len(predicted_temperature))
+print("The absolute men error is ", sum(abs(error))/len(predicted_temperature))
