Correcting notation of moving average days to moving average SPs (it was wrong before).

Data_Preprocessing/For_Single_Step_Prediction/Get_Features_And_Label.py
@@ -4,23 +4,25 @@ from sklearn.impute import SimpleImputer
 import matplotlib.pyplot as plt
 import datetime as dt
 
-# This csv data has missing values for the last 152 days of the year as they lie in the future. (Get rid of them)
-df = pd.read_csv("Data_Preprocessing/Load_GB_Processed_Data")
-df = df.rename(columns={"Unnamed: 0": "Timestamp", "0": "Load"}, errors="raise")
+df = pd.read_csv("Data_Preprocessing/Load_and_Transmission_Data.csv")
+df = df.rename(columns={"Unnamed: 0": "Timestamp"}, errors="raise")
 
 df["Timestamp"] = [dt.datetime.strptime(df.iloc[i,0][0:16], '%Y-%m-%d %H:%M') for i in range(len(df))]
+df["Time"] = df["Timestamp"]
+df = df.set_index(["Time"])
 
 fig1, axs1=plt.subplots(1,1,figsize=(12,6))
-axs1.plot(df.iloc[:,0], df.iloc[:,1])
-axs1.set_ylabel("Load in UK [MW]")
-axs1.set_xlabel("Date")
+axs1.plot(df.iloc[:,1], color = "blue", linewidth = 0.5)
+axs1.set_ylabel("Load in UK [MW]", size = 14)
+axs1.set_xlabel("Date", size = 14)
 axs1.grid(True)
-fig1.suptitle("Electricity Load in the UK from January 2016 to July 2020",fontsize=15)
+#fig1.suptitle("Electricity Load in the UK from January 2016 to July 2020",fontsize=15)
 fig1.show()
 
 # Determine the Features.
 df_features = pd.DataFrame()
 df_features["Load_Past"] = df["Load"].shift(+2)
+df_features["Transmission_Past"] = df["Transmission"].shift(+2)
 
 # Create artificial features.
 rolling_mean_10 = df_features["Load_Past"].rolling(window=10).mean()
@@ -28,10 +30,10 @@ rolling_mean_50 = df_features["Load_Past"].rolling(window=50).mean()
 exp_20 = df_features["Load_Past"].ewm(span=20, adjust=False).mean()
 exp_50 = df_features["Load_Past"].ewm(span=50, adjust=False).mean()
 
-df_features["Simple_Moving_Average_10_D"] = rolling_mean_10
-df_features["Simple_Moving_Average_50_D"] = rolling_mean_50
-df_features["Exp_Moving_Average_20_D"] = exp_20
-df_features["Exp_Moving_Average_50_D"] = exp_50
+df_features["Simple_Moving_Average_10_SP"] = rolling_mean_10
+df_features["Simple_Moving_Average_50_SP"] = rolling_mean_50
+df_features["Exp_Moving_Average_20_SP"] = exp_20
+df_features["Exp_Moving_Average_50_SP"] = exp_50
 
 # Create date relevant features.
 df_features["Settlement Period"] = df['Timestamp'].dt.hour*2+1+df['Timestamp'].dt.minute/30
@@ -49,10 +51,11 @@ replace_nan = SimpleImputer(missing_values=np.nan, strategy='mean')
 replace_nan.fit(X.iloc[:,:-1])
 X.iloc[:,:-1] = replace_nan.transform(X.iloc[:,:-1])
 
-y = df
+y = pd.DataFrame({"Load": df.iloc[:,1]})
 
-X.to_csv("Data_Preprocessing/For_Multi_Step_Prediction/X.csv")
-y.to_csv("Data_Preprocessing/For_Multi_Step_Prediction/y.csv")
+X.to_csv("Data_Preprocessing/For_Single_Step_Prediction/X.csv")
+y.to_csv("Data_Preprocessing/For_Single_Step_Prediction/y.csv")
+
 #
 # plt.plot(X[:,0], label='Electricity Generation 2 SP ago', linewidth=0.5 )
 # plt.xlabel("Actual Settlement Period")
@@ -62,5 +65,6 @@ y.to_csv("Data_Preprocessing/For_Multi_Step_Prediction/y.csv")
 # #plt.plot(X[-48*3:,2], label='50 Day SMA', color='black',  linewidth=0.5 )
 # #plt.plot(X[-48*3:,3], label='10 Day Exp MA', color='red',  linewidth=0.5 )
 # #plt.plot(X[-48*3:,4], label='50 Day Exp MA', color='red',  linewidth=0.5 )
+# #axs1.grid(True)
 # plt.legend()
 # plt.show()