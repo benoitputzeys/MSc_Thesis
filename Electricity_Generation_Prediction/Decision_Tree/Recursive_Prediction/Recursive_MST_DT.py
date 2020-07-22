060d57fc72a5e334c08163fbf764b45c3291fd0d
Benoit <59974364+benoitputzeys@users.noreply.github.com>
2020-07-22 16:13:55 +0200

    Improving the recursive prediction method by not including date-related features.

--- Electricity_Generation_Prediction/Decision_Tree/Recursive_Prediction/Recursive_MST_DT.py
+++ Electricity_Generation_Prediction/Decision_Tree/Recursive_Prediction/Recursive_MST_DT.py
@@ -6,20 +6,28 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_absolute_error, mean_squared_error
 import pandas as pd
 from pandas import DataFrame
+import matplotlib.ticker as plticker
 
 ########################################################################################################################
 # Get data and data preprocessing.
 ########################################################################################################################
 
-from numpy import genfromtxt
-
 # Get the X (containing the features) and y (containing the labels) values
-X = genfromtxt('Data_Entsoe/Data_Preprocessing/X.csv', delimiter=',')
-y = genfromtxt('Data_Entsoe/Data_Preprocessing/y.csv', delimiter=',')
-y = np.reshape(y, (len(y), 1))
+X = pd.read_csv('Data_Preprocessing/For_Single_Step_Prediction/X.csv', delimiter=',')
+X = X.set_index("Time")
+X = X.drop(columns = "Transmission_Past")
+dates = X.iloc[:,-1]
+X = X.iloc[:,:-5]
+
+y = pd.read_csv('Data_Preprocessing/For_Single_Step_Prediction/y.csv', delimiter=',')
+y = y.set_index("Time")
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0, shuffle = False)
-# Save the unscaled data for later for data representation.
+
+X_train = X_train[int(len(X_train)*1/2):]
+y_train = y_train[int(len(y_train)*1/2):]
+dates = dates[-len(X_train)-len(X_test):]
+
 X_test_unscaled = X_test
 X_train_unscaled = X_train
 
@@ -29,7 +37,6 @@ y_scaler = StandardScaler()
 X_train = x_scaler.fit_transform(X_train)
 X_test = x_scaler.transform(X_test)
 y_train = y_scaler.fit_transform(y_train)
-y_test = y_scaler.transform(y_test)
 
 ########################################################################################################################
 # Create the model.
@@ -39,107 +46,92 @@ y_test = y_scaler.transform(y_test)
 regressor = DecisionTreeRegressor(random_state = 0)
 regressor.fit(X_train, y_train)
 
-# Single-Step prediction
-result_train = y_scaler.inverse_transform(regressor.predict(X_train))
-result_test = y_scaler.inverse_transform(regressor.predict(X_test))
-
-result_test = result_test.reshape((len(result_test), 1))
-result_train = result_train.reshape((len(result_train), 1))
+########################################################################################################################
+# Predicting the generation.
+########################################################################################################################
 
 # Multi-Step prediction
-
-X_future_features = pd.DataFrame(data=X_test_unscaled,  columns=["0","1","2","3","4","5"])
-DoW_SP = genfromtxt('Data_Entsoe/Data_Preprocessing/For_Multi_Step_Prediction/DoW_SP_2.csv',delimiter=',')
-
-result_future = y_scaler.inverse_transform(y_test)
+X_future_features = pd.DataFrame(data=X_train_unscaled.iloc[-49:,:].values,  columns=["0","1","2","3","4","5"])
+result_future = y_scaler.inverse_transform(y_train[-2:])
 
 for i in range(0,48*7):
 
-    prev_value = result_future[-1]
-
+    prev_value = result_future[-2]
     new_row = [[prev_value[0], 0, 0, 0, 0, 0]]
-    new_row = DataFrame(new_row, columns=["0", "1", "2", "3", "4", "5"])
-    X_future_features = pd.concat([X_future_features,new_row], axis=0)
+    new_row = DataFrame(new_row, columns=["0","1","2","3","4","5"])
 
+    X_future_features = pd.concat([X_future_features,new_row])
     rolling_mean_10 = X_future_features["0"].rolling(window=10).mean().values[-1]
     rolling_mean_50 = X_future_features["0"].rolling(window=50).mean().values[-1]
     exp_20 = X_future_features["0"].ewm(span=20, adjust=False).mean().values[-1]
     exp_50 = X_future_features["0"].ewm(span=50, adjust=False).mean().values[-1]
 
-    update_row = [[prev_value, rolling_mean_10, rolling_mean_50, exp_20, exp_50, DoW_SP[i]]]
+    update_row = [[prev_value,
+                   rolling_mean_10,
+                   rolling_mean_50,
+                   exp_20, exp_50,
+                   X_test_unscaled.iloc[i,5]
+                   ]]
 
-    update_row = DataFrame(update_row, columns=["0", "1", "2", "3", "4", "5"])
+    update_row = DataFrame(update_row, columns=["0","1","2","3","4","5"])
     X_future_features.iloc[-1,:] = update_row.iloc[0,:]
 
-    result_future = np.append(result_future, y_scaler.inverse_transform(regressor.predict(x_scaler.transform(update_row))))
+    result_future = np.append(result_future, y_scaler.inverse_transform(regressor.predict(x_scaler.transform(update_row).reshape(1,6))))
     result_future = np.reshape(result_future,(-1,1))
 
-print("-"*200)
-error_train = result_train - y_scaler.inverse_transform(y_train)
-print("The mean absolute error of the training set is %0.2f" % mean_absolute_error(y_scaler.inverse_transform(y_train),result_train))
-print("The mean squared error of the training set is %0.2f" % mean_squared_error(y_scaler.inverse_transform(y_train),result_train))
-print("The root mean squared error of the training set is %0.2f" % np.sqrt(mean_squared_error(y_scaler.inverse_transform(y_train),result_train)))
+result_future = result_future[2:]
+
+########################################################################################################################
+# Inverse the scaling.
+########################################################################################################################
+
+X_train = x_scaler.inverse_transform(X_train)
+X_test = x_scaler.inverse_transform(X_test)
+y_train = y_scaler.inverse_transform(y_train)
+
+########################################################################################################################
+# Compute and print the errors.
+########################################################################################################################
 
 print("-"*200)
-error_test = result_test - y_scaler.inverse_transform(y_test)
-print("The mean absolute error of the training set is %0.2f" % mean_absolute_error(y_scaler.inverse_transform(y_test),result_test))
-print("The mean squared error of the training set is %0.2f" % mean_squared_error(y_scaler.inverse_transform(y_test),result_test))
-print("The root mean squared error of the training set is %0.2f" % np.sqrt(mean_squared_error(y_scaler.inverse_transform(y_test),result_test)))
+error_test = abs(result_future - y_test[:48*7])
+print("The mean absolute error of the prediction is %0.2f" % mean_absolute_error(y_test[:48*7],result_future[-48*7:]))
+print("The mean squared error of the prediction is %0.2f" % mean_squared_error(y_test[:48*7],result_future[-48*7:]))
+print("The root mean squared error of prediction set is %0.2f" % np.sqrt(mean_squared_error(y_test[:48*7],result_future[-48*7:])))
 print("-"*200)
 
 ########################################################################################################################
 # Visualising the results
 ########################################################################################################################
 
-fig1, ax1 = plt.subplots(3)
-fig1.suptitle('Decision Tree: Training Set', fontsize=16)
-ax1[0].plot(X_train_unscaled[:,0],linewidth=0.5)
-ax1[0].set_xlabel('Settlement Period')
-ax1[0].set_ylabel('Actual Value: Training Set')
+error_test_plot = np.zeros((48*3+48*7,1))
+error_test_plot[-336:] = error_test[:48*7]
+# Plot the result with the truth in red and the predictions in blue.
+fig2, axs2=plt.subplots(2,1,figsize=(12,6))
+axs2[0].grid(True)
+axs2[0].plot(dates.iloc[-len(X_test)-48*3:-len(X_test)],y_train[-48*3:,0]/1000, label = "Training Set (True Values)", alpha = 1, color = "black")
+axs2[0].plot(dates.iloc[-len(X_test):-len(X_test)+48*7],result_future[:48*7,0]/1000, label = "Decision T. Recursive Pred.", color = "orange")
+axs2[0].plot(dates.iloc[-len(X_test):-len(X_test)+48*7],y_test[:48*7]/1000, label = "Test Set (True Values)", alpha = 1, color = "blue")
+axs2[0].axvline(dates.iloc[-len(X_test)], linestyle="--", color = "black")
+axs2[0].set_ylabel('Load [GW]',size = 14)
+loc = plticker.MultipleLocator(base=47) # this locator puts ticks at regular intervals
+axs2[0].xaxis.set_major_locator(loc)
 
-ax1[1].plot(result_train, linewidth=0.5)
-ax1[1].set_xlabel('Settlement Period')
-ax1[1].set_ylabel('Single-Step Prediction on training set')
+axs2[1].grid(True)
+axs2[1].plot(dates.iloc[-len(X_test)-48*3:-len(X_test)+48*7], error_test_plot/1000, label = "Absolute error", alpha = 1, color = "red")
+axs2[1].axvline(dates.iloc[-len(X_test)], linestyle="--", color = "black")
+axs2[1].set_xlabel('Date',size = 14)
+axs2[1].set_ylabel('Absolute Error [GW]',size = 14)
+loc = plticker.MultipleLocator(base=47) # this locator puts ticks at regular intervals
+axs2[1].xaxis.set_major_locator(loc)
+fig2.autofmt_xdate(rotation=15)
 
-ax1[2].plot(abs(error_train), linewidth=0.5)
-ax1[2].set_xlabel('Settlement Period')
-ax1[2].set_ylabel('Absolute error: Training set')
-plt.show()
+axs2[1].legend(loc=(1.04,0.9))
+axs2[0].legend(loc=(1.04,0.7))
 
-fig2, ax2 = plt.subplots(3)
-fig2.suptitle('Decision Tree: Testing Set', fontsize=16)
-ax2[0].plot(X_test_unscaled[:,0], linewidth=0.5)
-ax2[0].set_xlabel('Settlement Period')
-ax2[0].set_ylabel('Single-Step Actual Value: Test Set')
-
-ax2[1].plot(result_test[:-48*7],linewidth=0.5)
-ax2[1].set_xlabel('Settlement Period')
-ax2[1].set_ylabel('Prediction on test set')
-
-ax2[2].plot(abs(error_test), linewidth=0.5)
-ax2[2].set_xlabel('Settlement Period')
-ax2[2].set_ylabel('Absolute error: Test set.')
-plt.show()
-
-figure3 = plt.figure(3)
-plt.plot(y_scaler.inverse_transform(result_future)[-48*7:], linewidth=0.5)
-plt.title('Prediction 7 days in the future')
-plt.xlabel('Settlement Period')
-plt.ylabel('Prediction')
-plt.show()
-
-values = X_future_features-X_future_features.shift(-1)
+fig2.show()
 
 ########################################################################################################################
-# Save the results in a csv file.
+# Results for recursive prediction are not saved because in general they yield poor results.
 ########################################################################################################################
 
-import csv
-with open('Compare_Models/SST_results/Decision_Tree_result.csv', 'w', newline='', ) as file:
-    writer = csv.writer(file)
-    writer.writerow(["Method","MSE","MAE","RMSE"])
-    writer.writerow(["Decision_Tree",
-                     str(mean_squared_error(y_scaler.inverse_transform(y_test),result_test)),
-                     str(mean_absolute_error(y_scaler.inverse_transform(y_test),result_test)),
-                     str(np.sqrt(mean_squared_error(y_scaler.inverse_transform(y_test),result_test)))
-                     ])
--- Electricity_Generation_Prediction/Random_Forest/Recursive_Prediction/Recursive_MST_RF.py
+++ Electricity_Generation_Prediction/Random_Forest/Recursive_Prediction/Recursive_MST_RF.py
@@ -4,39 +4,32 @@ from matplotlib import pyplot as plt
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_absolute_error, mean_squared_error
-from pandas import DataFrame
 import pandas as pd
-import pickle
-import datetime
-
-def create_dates(features_df, y_values):
-
-    date_list = [datetime.datetime(year=int(features_df[i, -1]),
-                                   month=int(features_df[i, -2]),
-                                   day=int(features_df[i, -3]),
-                                   hour=int((features_df[i, -4] - 1) / 2),
-                                   minute=int(((round(features_df[i, -4]) - 1) % 2) * 30)) for i in range(len(features_df))]
-    df_dates = DataFrame(date_list, columns=['Date'])
-    df_dates = df_dates.set_index(['Date'])
-    df_dates['Load'] = y_values
-
-    return df_dates
+from pandas import DataFrame
+import matplotlib.ticker as plticker
 
 ########################################################################################################################
 # Get data and data preprocessing.
 ########################################################################################################################
 
-from numpy import genfromtxt
-
 # Get the X (containing the features) and y (containing the labels) values
-X = genfromtxt('Data_Entsoe/Data_Preprocessing/X.csv', delimiter=',')
-y = genfromtxt('Data_Entsoe/Data_Preprocessing/y.csv', delimiter=',')
-y = np.reshape(y, (len(y), 1))
+X = pd.read_csv('Data_Preprocessing/For_Single_Step_Prediction/X.csv', delimiter=',')
+X = X.set_index("Time")
+X = X.drop(columns = "Transmission_Past")
+dates = X.iloc[:,-1]
+X = X.iloc[:,:-5]
+
+y = pd.read_csv('Data_Preprocessing/For_Single_Step_Prediction/y.csv', delimiter=',')
+y = y.set_index("Time")
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0, shuffle = False)
+
+X_train = X_train[int(len(X_train)*1/2):]
+y_train = y_train[int(len(y_train)*1/2):]
+dates = dates[-len(X_train)-len(X_test):]
+
 X_test_unscaled = X_test
 X_train_unscaled = X_train
-
 
 # Feature Scaling
 x_scaler = StandardScaler()
@@ -44,36 +37,28 @@ y_scaler = StandardScaler()
 X_train = x_scaler.fit_transform(X_train)
 X_test = x_scaler.transform(X_test)
 y_train = y_scaler.fit_transform(y_train)
-y_test = y_scaler.transform(y_test)
 
 ########################################################################################################################
 # Create the model.
 ########################################################################################################################
 
-# # Fit the Random Forest to our data
-# regressor = RandomForestRegressor(n_estimators=200, random_state=0)
-# regressor.fit(X_train, y_train)
+# Fit the Random Forest to our data
+regressor = RandomForestRegressor(n_estimators=200, random_state=0)
+regressor.fit(X_train, y_train)
 
-regressor = pickle.load(open("Electricity_Generation_Prediction/Random_Forest/my_model.sav", 'rb'))
-
-intermediate_result_test_prediction = regressor.predict(X_test)
-intermediate_result_train_prediction = regressor.predict(X_train)
-
-result_test = y_scaler.inverse_transform(intermediate_result_test_prediction)
-result_train = y_scaler.inverse_transform(intermediate_result_train_prediction)
-
-result_test = result_test.reshape((len(result_test), 1))
-result_train = result_train.reshape((len(result_train), 1))
+########################################################################################################################
+# Predicting the generation.
+########################################################################################################################
 
 # Multi-Step prediction
-X_future_features = pd.DataFrame(data=X_train_unscaled[-49:,:],  columns=["0","1","2","3","4","5","6","7","8","9"])
+X_future_features = pd.DataFrame(data=X_train_unscaled.iloc[-49:,:].values,  columns=["0","1","2","3","4","5"])
 result_future = y_scaler.inverse_transform(y_train[-2:])
 
 for i in range(0,48*7):
 
     prev_value = result_future[-2]
-    new_row = [[prev_value[0], 0, 0, 0, 0, 0, 0, 0, 0, 0]]
-    new_row = DataFrame(new_row, columns=["0","1","2","3","4","5","6","7","8","9"])
+    new_row = [[prev_value[0], 0, 0, 0, 0, 0]]
+    new_row = DataFrame(new_row, columns=["0","1","2","3","4","5"])
 
     X_future_features = pd.concat([X_future_features,new_row])
     rolling_mean_10 = X_future_features["0"].rolling(window=10).mean().values[-1]
@@ -81,54 +66,72 @@ for i in range(0,48*7):
     exp_20 = X_future_features["0"].ewm(span=20, adjust=False).mean().values[-1]
     exp_50 = X_future_features["0"].ewm(span=50, adjust=False).mean().values[-1]
 
-    update_row = [[prev_value, rolling_mean_10, rolling_mean_50, exp_20, exp_50, X_test_unscaled[i,5], X_test_unscaled[i,6], X_test_unscaled[i,7], X_test_unscaled[i,8], X_test_unscaled[i,9]]]
+    update_row = [[prev_value,
+                   rolling_mean_10,
+                   rolling_mean_50,
+                   exp_20, exp_50,
+                   X_test_unscaled.iloc[i,5]
+                   ]]
 
-    update_row = DataFrame(update_row, columns=["0","1","2","3","4","5","6","7","8","9"])
+    update_row = DataFrame(update_row, columns=["0","1","2","3","4","5"])
     X_future_features.iloc[-1,:] = update_row.iloc[0,:]
 
-    result_future = np.append(result_future, y_scaler.inverse_transform(regressor.predict(x_scaler.transform(update_row))))
+    result_future = np.append(result_future, y_scaler.inverse_transform(regressor.predict(x_scaler.transform(update_row).reshape(1,6))))
     result_future = np.reshape(result_future,(-1,1))
 
 result_future = result_future[2:]
 
 ########################################################################################################################
-# Data processing for plotting curves and printing the errors.
+# Inverse the scaling.
+########################################################################################################################
+
+X_train = x_scaler.inverse_transform(X_train)
+X_test = x_scaler.inverse_transform(X_test)
+y_train = y_scaler.inverse_transform(y_train)
+
+########################################################################################################################
+# Compute and print the errors.
 ########################################################################################################################
 
 print("-"*200)
-error_test = abs(result_future - y_scaler.inverse_transform(y_test[:48*7]))
-print("The mean absolute error of the prediction is %0.2f" % mean_absolute_error(y_scaler.inverse_transform(y_test[:48*7]),result_future[-48*7:]))
-print("The mean squared error of the prediction is %0.2f" % mean_squared_error(y_scaler.inverse_transform(y_test[:48*7]),result_future[-48*7:]))
-print("The root mean squared error of prediction set is %0.2f" % np.sqrt(mean_squared_error(y_scaler.inverse_transform(y_test[:48*7]),result_future[-48*7:])))
+error_test = abs(result_future - y_test[:48*7])
+print("The mean absolute error of the prediction is %0.2f" % mean_absolute_error(y_test[:48*7],result_future[-48*7:]))
+print("The mean squared error of the prediction is %0.2f" % mean_squared_error(y_test[:48*7],result_future[-48*7:]))
+print("The root mean squared error of prediction set is %0.2f" % np.sqrt(mean_squared_error(y_test[:48*7],result_future[-48*7:])))
 print("-"*200)
 
 ########################################################################################################################
 # Visualising the results
 ########################################################################################################################
 
-y_values_dates = create_dates(X_future_features[-48*7:].to_numpy(),result_future)
-figure1 = plt.figure(5)
-plt.plot(y_values_dates, linewidth=0.5)
-plt.title('Prediction 7 days in the future with RF')
-plt.xlabel('Settlement Period')
-plt.ylabel('Electricity Load [MW]')
+error_test_plot = np.zeros((48*3+48*7,1))
+error_test_plot[-336:] = error_test[:48*7]
+# Plot the result with the truth in red and the predictions in blue.
+fig2, axs2=plt.subplots(2,1,figsize=(12,6))
+axs2[0].grid(True)
+axs2[0].plot(dates.iloc[-len(X_test)-48*3:-len(X_test)],y_train[-48*3:,0]/1000, label = "Training Set (True Values)", alpha = 1, color = "black")
+axs2[0].plot(dates.iloc[-len(X_test):-len(X_test)+48*7],result_future[:48*7,0]/1000, label = "Random F. Recursive Pred.", color = "orange")
+axs2[0].plot(dates.iloc[-len(X_test):-len(X_test)+48*7],y_test[:48*7]/1000, label = "Test Set (True Values)", alpha = 1, color = "blue")
+axs2[0].axvline(dates.iloc[-len(X_test)], linestyle="--", color = "black")
+axs2[0].set_ylabel('Load [GW]',size = 14)
+loc = plticker.MultipleLocator(base=47) # this locator puts ticks at regular intervals
+axs2[0].xaxis.set_major_locator(loc)
 
-fig2, axes2 = plt.subplots(2)
-axes2[0].plot(y_values_dates, linewidth=0.5, label ="Prediction 7 days in the future with RF")
-axes2[0].set_xlabel("Settlement Period")
-axes2[0].set_ylabel("Electricity Load [MW]")
-y_values_dates = create_dates(X_future_features[-48*7:].to_numpy(), y_scaler.inverse_transform(y_test[:48*7]))
-axes2[0].plot(y_values_dates,linewidth=0.5, label="Actual")
+axs2[1].grid(True)
+axs2[1].plot(dates.iloc[-len(X_test)-48*3:-len(X_test)+48*7], error_test_plot/1000, label = "Absolute error", alpha = 1, color = "red")
+axs2[1].axvline(dates.iloc[-len(X_test)], linestyle="--", color = "black")
+axs2[1].set_xlabel('Date',size = 14)
+axs2[1].set_ylabel('Absolute Error [GW]',size = 14)
+loc = plticker.MultipleLocator(base=47) # this locator puts ticks at regular intervals
+axs2[1].xaxis.set_major_locator(loc)
+fig2.autofmt_xdate(rotation=15)
 
-y_values_dates = create_dates(X_future_features[-48*7:].to_numpy(),error_test)
-fig2.legend()
-axes2[1].plot(y_values_dates,linewidth=0.5, label ="Absolute Error",color= "black")
-axes2[1].set_xlabel("Settlement Period")
-axes2[1].set_ylabel("Error in Prediction [MW]")
-fig2.legend()
+axs2[1].legend(loc=(1.04,0.9))
+axs2[0].legend(loc=(1.04,0.7))
+
+fig2.show()
 
 ########################################################################################################################
-# Save the results in a csv file.
+# Results for recursive prediction are not saved because in general they yield poor results.
 ########################################################################################################################
 
-pd.DataFrame(result_future).to_csv("Electricity_Generation_Prediction/Hybrid_Model/Pred_train2_other_metrics/RF_prediction.csv")
