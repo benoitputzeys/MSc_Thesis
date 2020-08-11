f89d62689a125d4d83738cf57056c0dcfc486f64
Benoit <59974364+benoitputzeys@users.noreply.github.com>
2020-08-11 22:28:22 +0200

    Include a file to explain regression, the working principle of a decision tree, a random forest, SVR and a visualisation to see how the evidence and prior influence the posterior.

---
+++ Electricity_Generation_Prediction/Random_Forest/RF_Explained/RF_Explained.py
@@ -0,0 +1,37 @@
+# Import the necessary modules and libraries
+# Code mostly taken from: https://scikit-learn.org/stable/auto_examples/tree/plot_tree_regression.html
+import numpy as np
+from sklearn.ensemble import RandomForestRegressor
+import matplotlib.pyplot as plt
+from sklearn import tree
+
+# Create a random dataset
+rng = np.random.RandomState(1)
+X = np.sort(5 * rng.rand(80, 1), axis=0)
+y = np.sin(X).ravel()
+y[::5] += 3 * (0.5 - rng.rand(16))
+
+# Fit regression model
+regr_1 = RandomForestRegressor(n_estimators=5, random_state=0, max_depth = 4)
+regr_2 = RandomForestRegressor(n_estimators=100, random_state=0, max_depth = 4)
+regr_1.fit(X, y)
+regr_2.fit(X, y)
+
+# Predict
+X_test = np.arange(0.0, 5.0, 0.01)[:, np.newaxis]
+y_1 = regr_1.predict(X_test)
+y_2 = regr_2.predict(X_test)
+
+# Plot the results
+fig1, axs1=plt.subplots(1,1,figsize=(8,6))
+axs1.scatter(X, y, s=20, color="blue", label="Datapoint")
+axs1.plot(X_test, y_1, color="orange",label="RF Prediction\nNumber of Trees = 5", linewidth=2)
+axs1.plot(X_test, y_2, color="yellowgreen", label="RF Prediction\nNumber of Trees = 100", linewidth=2)
+axs1.set_xlabel("x", size = 14)
+axs1.set_ylabel("f(x)", size = 14)
+axs1.set_title("Random Forest Regression", size = 14)
+axs1.grid(True)
+axs1.tick_params(axis = "both", labelsize = 14)
+axs1.legend()
+fig1.show()
+fig1.savefig("Electricity_Generation_Prediction/Random_Forest/Figures/Random_Forest_Explained.pdf", bbox_inches='tight')
---
+++ TF_Probability/MST_Rd_Weights/Intuition_Prio_Posterior_Distributions.py
@@ -0,0 +1,29 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.stats as stats
+import math
+
+mu_prior = 0
+variance_prior = 1.5
+sigma_prior = math.sqrt(variance_prior)
+
+mu_evidence = -3
+variance_evidence = 0.75
+sigma_evidence = math.sqrt(variance_evidence)
+
+mu_posterior = -2
+variance_posterior = 1
+sigma_posterior = math.sqrt(variance_posterior)
+
+x_axis = np.linspace(-9, 5, 100)
+fig, axs=plt.subplots(1,1,figsize=(12,6))
+axs.plot(x_axis, stats.norm.pdf(x_axis, mu_prior, sigma_prior), linewidth = 2, label = "Prior Distribution P(A)", color = "red")
+axs.plot(x_axis, stats.norm.pdf(x_axis, mu_evidence, sigma_evidence), linewidth = 2, label = "Evidence P(B)", color = "blue")
+axs.plot(x_axis, stats.norm.pdf(x_axis, mu_posterior, sigma_posterior), linewidth = 2, label = "Posterior Distribution P(A|B)", color = "orange")
+#axs.grid(b=True, which='major'), axs.grid(b=True, which='minor',alpha = 0.2)
+#axs.tick_params(axis = "both", labelsize = 12)
+#axs.minorticks_on()
+axs.set_xticks([]), axs.set_yticks([])
+axs.legend(fontsize=14)
+fig.show()
+fig.savefig("TF_Probability/MST_Rd_Weights/Figures/Visual_Intuition_Prior_and_Posterior.pdf", bbox_inches='tight')
